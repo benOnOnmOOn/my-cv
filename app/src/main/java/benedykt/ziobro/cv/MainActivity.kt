15ad92b14cbdb43948763118302c07617d1911a4
bziobro <benedykt.ziobro@billennium.com>
2020-09-08 19:45:28 +0200

    Init repository

---
+++ app/src/main/java/benedykt/ziobro/cv/MainActivity.kt
@@ -0,0 +1,11 @@
+package benedykt.ziobro.cv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
